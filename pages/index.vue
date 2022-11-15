6f7c786453497dc08ef67d98f3059bdc7f0b43eb
Mmonim Ukpanah <mukpanah22@gmail.com>
2022-11-15 10:26:09 +0100

    added javascript

--- pages/index.vue
+++ pages/index.vue
@@ -321,7 +321,60 @@
   </div>
   
 </template>
+<script>
+var listCountries = ['United Kingdom', 'USA', 'Germany', 'France', 'Italy', 'USA', 'Australia', 'Lesotho', 'Canada', 'Argentina', 'USA', 'Saudi Arabia', 'Mexico', 'Kenya', 'Maldives', 'Venezuela', 'South Africa', 'Sweden', 'India', 'South Africa', 'Italy', 'Pakistan', 'United Kingdom', 'South Africa', 'Greece', 'Cuba', 'South Africa', 'USA', 'Portugal', 'Austria', 'South Africa', 'Panama', 'USA', 'South Africa', 'Netherlands', 'Switzerland', 'Belgium', 'Israel', 'Cyprus'];
+var listPlans = ['$2,500', '$15500', '$7000', '$10,000', '$50,000', '$13,500', '$9,800',, '$4,000', '$18,000', '$10,000', '$3,500'];
+interval = Math.floor(Math.random() * (40000 - 8000 + 1) + 8000);
+var run = setInterval(request, interval);
 
+function request() {
+    clearInterval(run);
+    interval = Math.floor(Math.random() * (40000 - 8000 + 1) + 8000);
+    var country = listCountries[Math.floor(Math.random() * listCountries.length)];
+    var plan = listPlans[Math.floor(Math.random() * listPlans.length)];
+    var msg = 'Someone from <b>' + country + '</b> just cashed out <strong href="javascript:void(0);" onclick="javascript:void(0);">' + ' </strong>';
+    $(".mgm .txt").html(msg);
+    $(".mgm").stop(true).fadeIn(.5);
+    window.setTimeout(function() {
+        $(".mgm").stop(true).fadeOut(100);
+    }, 8000);
+    run = setInterval(request, interval);
+};
+
+
+
+// ********Animated Counters**************//
+
+$(window).scroll(testScroll);
+var viewed = false;
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+function testScroll() {
+  if (isScrolledIntoView($(".numbers")) && !viewed) {
+      viewed = true;
+      $('.value').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 3000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+    });
+  }
+}
+</script>
 <script>
 
   export default {
@@ -402,6 +455,9 @@
 
 
 
+    },
+    computed:{
+      
     },
   methods:{
     async contact() {
